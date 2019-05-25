970b94d331afc826e0b017b3451902c75ba9155b
Kekstantin <50292649+Kekstantin@users.noreply.github.com>
2019-05-25 11:08:35 +0300

    Add files via upload

--- list_of_types.h
+++ list_of_types.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <list>
+#include <algorithm>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+
+enum class_of_the_monster_ {water_, earth_, air_, fire_, lighting_};
+enum type_of_the_monster_ {slime_, goblin_, scp_173_};
+enum type_of_the_weapon_ {knife_, sword_, bow_, mace_};
+
+using damage_ = int;
+using health_ = int;
+using balance_ = int;
