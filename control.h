abcde57c98731c043efa0929a75ca738a6f95b73
Kekstantin <50292649+Kekstantin@users.noreply.github.com>
2019-06-01 11:20:49 +0300

    Update control.h

--- control.h
+++ control.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "C:\Users\Äìèòðèé\Downloads\quest-team-work-master\quest-team-work-master\door.h"
+#include "C:\Users\Ã„Ã¬Ã¨Ã²Ã°Ã¨Ã©\Downloads\quest-team-work-master\quest-team-work-master\door.h"
 
 class control
 {
@@ -10,20 +10,28 @@ public:
 
 private:
 	int _stage = 0;
-	damage_ _start_dmg_for_slime = 5;
-	damage_ _start_dmg_for_goblin = 15;
-	damage_ _start_dmg_for_scp_173 = 50;
+	damage_ _dmg_for_slime = 5;
+	damage_ _dmg_for_goblin = 15;
+	damage_ _dmg_for_scp_173 = 50;
+
 	const damage_ _knife_dmg = 10;
 	const damage_ _mace_dmg = 20;
 	const damage_ _sword_dmg = 30;
 	const damage_ _bow_dmg = 50;
-	balance_ _start_steal_balance_slime = 5;
-	balance_ _start_steal_balance_goblin = 15;
-	balance_ _start_steal_balance_scp_173 = 40;
-	balance_ _start_loose_balance_slime = _start_steal_balance_slime * 1.5;
-	balance_ _start_loose_balance_goblin = _start_steal_balance_goblin * 1.5;
-	balance_ _start_loose_balance_scp_173 = _start_steal_balance_scp_173 * 1.5;
+
+	const balance_ _balance_door = 10;
+
+	const balance_ _steal_balance_slime = 5;
+	const balance_ _steal_balance_goblin = 15;
+	const balance_ _steal_balance_scp_173 = 40;
+
+	balance_ _loose_balance_slime = _start_steal_balance_slime *1.5;
+	balance_ _loose_balance_goblin = _start_steal_balance_goblin * 1.5;
+	balance_ _loose_balance_scp_173 = _start_steal_balance_scp_173 * 1.5;
+
+	const int _multiplyer_for_loose_and_steal = 1.5;
 	const int _multiplyer_for_monsters = 1.2;
 	const int _multiplyer_for_rewards = 1.3;
+};
 };
 
