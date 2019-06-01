493ebd3a34dd83da77ee84f5871cec3424fbd67f
Kekstantin <50292649+Kekstantin@users.noreply.github.com>
2019-06-01 10:50:04 +0300

    Update monster_abs.cpp

--- monster_abs.cpp
+++ monster_abs.cpp
@@ -1,44 +1,40 @@
 #include "monster_abs.h"
 
-
-
 int monster_abs::get_sum_defeat() const
 {
-	return _sum_defeat;
+return _sum_defeat;
 }
 
 void monster_abs::set_sum_defeat(int sum_defeat)
 {
-	_sum_defeat = sum_defeat;
+_sum_defeat = sum_defeat;
 }
 
 int monster_abs::get_donate() const
 {
-	return _donate;
+return _donate;
 }
 
 void monster_abs::set_donate(int donate)
 {
-	_donate = donate;
+_donate = donate;
 }
 
-class_of_the_monster_ monster_abs::ger_monster_class() const
+class_of_the_monster_ monster_abs::get_monster_class() const
 {
-	return _monster_class;
+return _monster_class;
 }
 
-type_of_the_monster_ monster_abs::introduce()
+type_of_the_monster_ monster_abs::get_type_of_the_monster() const
 {
-	return _monster_type;
+return _monster_type;
 }
 
-monster_abs::monster_abs()
-{
+std::string introduce() const{
+return "Вы видите " + std::to_string(get_type_of_the_monster()) + "Типа " + std::to_string(get_monster_class());
+
 }
 
+monster_abs::monster_abs(){}
 
-monster_abs::~monster_abs()
-{
-}
-
-
+monster_abs::~monster_abs(){}
